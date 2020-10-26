784a7ad4c7896613ae8caa2d4df2bf6ef1069a99
horihorikeita <horihorikeita@gmail.com>
2020-10-27 05:39:58 +0900

    ‘second’

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,17 +14,15 @@
 //= require activestorage
 //= require_tree .
 
-document.addEventListener("turbolinks:load", function () {
-  const nav = document.getElementsByClassName("nav-js")[0];
-  const button = document.getElementsByClassName("js-nav-button")[0];
+const nav = document.getElementsByClassName("nav-js")[0];
+const button = document.getElementsByClassName("js-nav-button")[0];
 
-  button.addEventListener("click", () => {
-    nav.classList.toggle("active");
+button.addEventListener("click", () => {
+  nav.classList.toggle("active");
 
-    if (nav.classList.contains("active")) {
-      button.classList.add("active");
-    } else {
-      button.classList.remove("active");
-    }
-  });
+  if (nav.classList.contains("active")) {
+    button.classList.add("active");
+  } else {
+    button.classList.remove("active");
+  }
 });
